78f2c96c1cca809b63f7cf73317271cdd2b5d6b2
Andrew <diver.vlz@gmail.com>
2020-12-03 11:24:12 +0300

    task_5_les_4_applied_changes
    Another way to solution

--- Lesson_4/task_5.py
+++ Lesson_4/task_5.py
@@ -13,3 +13,7 @@ def mul_list(el_1, el_2):
 
 uniq_list = [el for el in range(100, 1001, 2)]
 print(f"List\n{uniq_list}\nMultiplication of numbers\n{reduce(mul_list, uniq_list)}")
+
+# другой вариант решения
+
+print(reduce(lambda a, b: a * b, [x for x in range(100, 1001, 2)]))
