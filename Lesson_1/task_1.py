3db3ca61bd2f1bc16e8c3f202bafbcfb1b78bacd
Andrew <diver.vlz@gmail.com>
2020-12-13 17:40:13 +0300

    Tasks 1-3 for lesson 1 added
    Tasks 1-3 complete

---
+++ Lesson_1/task_1.py
@@ -0,0 +1,13 @@
+"""урок 1 задание 1
+Поработайте с переменными, создайте несколько, выведите на экран, запросите у пользователя
+несколько чисел и строк и сохраните в переменные, выведите на экран.
+"""
+
+a = "hello"
+b = "world!"
+print(f"{a}, {b}")
+numb1 = int(input("Enter any number: "))
+numb2 = int(input("Enter any number one more time: "))
+print(f"You have chosen the numbers {numb1} and {numb2}")
+word = input("Enter any word: ")
+print(f"{word} - it's good choice")
